639e5245c5e2cb8dcbd806e7d1917d95ef5179f6
Dan Hert <hertdani@fel.cvut.cz>
2024-10-17 16:05:41 +0200

    F4F fork

---
+++ README.md
@@ -0,0 +1,54 @@
+# OpenMV Cam ROS package
+
+This package provides an interface between the [OpenMV Cam](https://openmv.io/) and [ROS](http://www.ros.org/).
+
+Images are acquired by the camera and sent via USB to the ROS node which relays them as ROS image messages.
+
+## Resolving dependencies
+
+To resolve the dependencies run:
+
+```sh
+rosdep install --from-paths /path/to/your/catkin_ws/src --ignore-src
+```
+
+**Note**:
+For ROS Noetic and later, the [`camera_info_manager_py`](https://github.com/ros-perception/camera_info_manager_py) package is not available via `apt-get` and has to be built from source.
+
+## Getting started
+
+Connect the camera and copy the [main.py](util/main.py) file, either using the [OpenMV IDE](https://openmv.io/pages/download) or directly using the mounted storage device.
+Remember to reset the camera via the IDE or eject the storage device to prevent data loss.
+
+Launch the node as follows:
+
+```sh
+roslaunch openmv_cam openmv_cam.launch
+```
+
+By default, the node uses the following parameters:
+
+```sh
+roslaunch openmv_cam openmv_cam.launch \
+    device:=/dev/ttyACM0 \
+    image:=image_raw \
+    camera:=camera_info \
+    calibration:=$(rospack find openmv_cam)/calib/standard_lens.yaml
+```
+
+## Camera calibration
+
+The [calib](calib/) folder contains calibrations for standard OpenMV lenses:
+
+* Standard lens (included with the camera)
+* [Ultra-wide angle lens](https://openmv.io/collections/lenses/products/ultra-wide-angle-lens)
+
+The calibrations were obtained using the ROS [camera_calibration](http://wiki.ros.org/camera_calibration) package.
+
+**Note**:
+In order to obtain accurate calibration results for wide-angle cameras, consider using the [OpenCV fisheye calibration model](https://docs.opencv.org/3.4.8/db/d58/group__calib3d__fisheye.html) or the *equidistant* model from the [Kalibr visual-inertial calibration toolbox](https://github.com/ethz-asl/kalibr).
+Both codebases rely on the [Kannala-Brandt calibration method](https://ieeexplore.ieee.org/document/1642666).
+
+## License
+
+The OpenMV Cam ROS package is released under the [MIT License](LICENSE.md).
---
+++ src/openmv_cam_node.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+"""Publishes OpenMV Cam images as ROS messages."""
+
+import sys
+
+import rospy
+from camera_info_manager import CameraInfoManager
+import cv2
+from cv_bridge import CvBridge
+from sensor_msgs.msg import CameraInfo, Image
+
+from openmv_cam import OpenMVCam
+
+CAMERA_NAME = 'openmv_cam'
+DEFAULT_IMAGE_TOPIC = 'image_raw'
+DEFAULT_CAMERA_TOPIC = 'camera_info'
+
+
+class OpenMVCamNode:
+
+    def __init__(self):
+
+        rospy.init_node('{}_node'.format(CAMERA_NAME), argv=sys.argv)
+
+        self.device = rospy.get_param('~device', '/dev/ttyACM0')
+        self.image_topic = rospy.get_param('~image', DEFAULT_IMAGE_TOPIC)
+        self.camera_topic = rospy.get_param('~camera', DEFAULT_CAMERA_TOPIC)
+        self.calibration = rospy.get_param('~calibration', '')
+
+        self.manager = CameraInfoManager(cname=CAMERA_NAME,
+                                         url='file://' + self.calibration,
+                                         namespace=CAMERA_NAME)
+
+        self.manager.loadCameraInfo()  # Needs to be called before getter!
+        self.camera_info = self.manager.getCameraInfo()
+
+        self.openmv_cam = OpenMVCam(self.device)
+        self.bridge = CvBridge()
+
+        self.image_publisher = rospy.Publisher(self.image_topic, Image,
+                                               queue_size=1)
+        self.camera_publisher = rospy.Publisher(self.camera_topic, CameraInfo,
+                                                queue_size=1)
+        self.seq = 0
+
+    def read_and_publish_image(self):
+
+        # Read image from camera
+        image = self.openmv_cam.read_image()
+
+        # Deduce color/grayscale from image dimensions
+        channels = 1 if image.ndim == 2 else 3
+        encoding = 'bgr8' if channels == 3 else 'mono8'
+
+        # Convert from BGR to RGB by reversing the channel order
+        if channels == 3:
+            image = image[..., ::-1]
+
+        # Convert numpy image to ROS image message
+        image_msg = self.bridge.cv2_to_imgmsg(image, encoding=encoding)
+
+        # Add timestamp and sequence number (empty by default)
+        image_msg.header.stamp = rospy.Time.now()
+        image_msg.header.seq = self.seq
+
+        self.image_publisher.publish(image_msg)
+        camera_msg = self.camera_info
+        camera_msg.header = image_msg.header  # Copy header from image message
+        self.camera_publisher.publish(camera_msg)
+
+        if self.seq == 0:
+            rospy.loginfo("Publishing images from OpenMV Cam at '{}' "
+                          .format(self.device))
+        self.seq += 1
+
+
+def main():
+
+    openmv_cam_node = OpenMVCamNode()
+
+    while not rospy.is_shutdown():
+        openmv_cam_node.read_and_publish_image()
+
+
+if __name__ == '__main__':
+    main()
